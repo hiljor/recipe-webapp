1638f26896f4f263beb797b650857ed60caf3788
Hilde Jordal <xat013@uib.no>
2026-02-24 13:32:16 +0100

    database seeding script complete and working

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,6 +1,6 @@
 import postgres from 'postgres';
 import bcrypt from 'bcrypt';
-import { recipes, users, recipeSeedingStrings, userSeedingStrings } from '../lib/example-data';
+import { recipes, users, recipeSeedingStatements, userSeedingStatements, resetStatements } from '../lib/example-data';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -12,12 +12,9 @@ async function runStatements(statements: string[][]) {
   for (const group of statements) {
     await Promise.all(
       group.map(async stmt => {
-        return sql`${stmt}`
+        return sql.unsafe(stmt)
       })
     )
-    for (const stmt of group) {
-
-    }
   }
 }
 
@@ -26,8 +23,9 @@ async function runStatements(statements: string[][]) {
  * @returns
  */
 async function seedUsers() {
-  await runStatements(userSeedingStrings);
+  await runStatements(userSeedingStatements);
 
+  console.log("inserting users")
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
@@ -42,52 +40,93 @@ async function seedUsers() {
   return insertedUsers;
 }
 
+async function addRecipe(
+  recipe: {
+    title: string,
+    author_id: number,
+    servings: number,
+    added: string,
+    updated: string,
+    ingredients: {
+      name: string, 
+      quantity: number, 
+      unit: string, 
+      pos: number, 
+    }[]
+    steps: {
+      type: string,
+      text: string,
+      pos: number
+    }[]
+  }
+){
+    // Insert primary recipe information
+    const recipeTableInsert = await sql`
+      INSERT INTO recipes (title, author_id, servings, added, updated)
+      VALUES (${recipe.title}, ${recipe.author_id}, ${recipe.servings}, ${recipe.added}, ${recipe.updated})
+      RETURNING id;`;
+      
+    const recipeId = recipeTableInsert[0]?.id;
+    
+  
+  return Promise.all(
+    [
+      Promise.all(
+        recipe.ingredients.map(async (ingredient) => {
+
+          // insert ingredient if new
+          const ingredientTableInsert = await sql`
+            INSERT INTO ingredients (name) VALUES (${ingredient.name})
+            ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
+            RETURNING id;
+            `
+          const ingredientId = ingredientTableInsert[0]?.id;
+          
+          // insert recipe ingredients
+          return sql`
+            INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity, unit, pos)
+            VALUES (${recipeId}, ${ingredientId}, ${ingredient.quantity}, ${ingredient.unit}, ${ingredient.pos});
+          `
+        })
+      )
+      ,
+      Promise.all(
+        recipe.steps.map(async (step) => {
+          return sql`INSERT INTO steps (recipe_id, type, text, pos)
+          VALUES (${recipeId}, ${step.type}, ${step.text}, ${step.pos});
+          `
+        })
+      )
+    ]
+  )
+}
+
 /**
  * Seeds the recipe database.
  */
 async function seedRecipes(){
-  await runStatements(recipeSeedingStrings);
+  await runStatements(recipeSeedingStatements);
 
-  const insertedRecipes = await Promise.all(
-    recipes.map(async (recipe) => {
-      return Promise.all([
-        sql`INSERT INTO recipes (id, title, author_id, servings, added, updated)
-        VALUES (${recipe.id}, ${recipe.title}, ${recipe.author_id}, ${recipe.servings}, ${recipe.added}, ${recipe.updated})
-        ON CONFLICT (id) DO NOTHING;
-        `,
-        Promise.all(
-          recipe.ingredients.map(async (ingredient) => {
-            return Promise.all([
-              sql`INSERT INTO ingredients (name) VALUES (${ingredient.name})
-              ON CONFLICT (name) DO NOTHING`,
-              sql`INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity, unit, pos)
-              VALUES (${recipe.id}, ${ingredient.id}, ${ingredient.quantity}, ${ingredient.unit}, ${ingredient.pos})
-              ON CONFLICT (id) DO NOTHING;
-              `
-            ])
-          })
-        )
-        ,
-        Promise.all(
-          recipe.steps.map(async (step) => {
-            return sql`INSERT INTO steps (recipe_id, type, text, pos)
-            VALUES (${recipe.id}, ${step.type}, ${step.text}, ${step.pos});
-            `
-        })
-        )
-      ]
-      )
-    })
+  return Promise.all(
+    recipes.map((recipe => {
+      return addRecipe(recipe);
+    }))
   )
+}
 
-  return insertedRecipes;
+/**
+ * Deletes 
+ */
+async function clearDB() {
+  return runStatements(resetStatements);
 }
 
 export async function GET() {
   try {
     const result = await sql.begin((sql) => [
+      //clearDB(),
       seedUsers(),
-      seedRecipes(),
+      seedRecipes()
     ]);
 
     return Response.json({ message: 'Database seeded successfully' });
