eec847dccee6e789916c6e813aa40f67db08942b
Hilde Jordal <xat013@uib.no>
2026-02-25 18:40:49 +0100

    RecipeCard component

---
+++ src/ui/recipes/RecipeCard.tsx
@@ -0,0 +1,21 @@
+import { Recipe } from "@/app/generated/prisma/client";
+import { RecipeDTO } from "@/lib/types";
+
+export function RecipeCard({
+  recipe
+}: {
+  recipe: RecipeDTO
+}) {
+
+  return (
+    <div className="mt-4" >
+      <h1 className="text-4xl text-black font-bold">{recipe.title}</h1>
+      <div className="mt-2 text-m text-gray-600 flex items-center space-x-4">
+        {recipe.authorName && <span>by {recipe.authorName}</span>}
+        {recipe.added && (
+          <span>{new Date(recipe.added).toLocaleDateString()}</span>
+        )}
+      </div>
+    </div>
+  );
+}
