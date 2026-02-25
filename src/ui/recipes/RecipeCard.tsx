4a4063d3f29fe99dadef790d1884763268f68005
Hilde Jordal <xat013@uib.no>
2026-02-25 18:44:48 +0100

    commit to resolve #7

--- src/ui/recipes/RecipeCard.tsx
+++ src/ui/recipes/RecipeCard.tsx
@@ -1,14 +1,9 @@
 import { Recipe } from "@/app/generated/prisma/client";
 import { RecipeDTO } from "@/lib/types";
 
-export function RecipeCard({
-  recipe
-}: {
-  recipe: RecipeDTO
-}) {
-
+export function RecipeCard({ recipe }: { recipe: RecipeDTO }) {
   return (
-    <div className="mt-4" >
+    <div className="mt-4">
       <h1 className="text-4xl text-black font-bold">{recipe.title}</h1>
       <div className="mt-2 text-m text-gray-600 flex items-center space-x-4">
         {recipe.authorName && <span>by {recipe.authorName}</span>}
