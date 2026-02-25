5abb7bd1153f7137edad44443cd08848af42b0d1
Hilde Jordal <xat013@uib.no>
2026-02-25 15:53:27 +0100

    add return types in data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,11 +1,11 @@
 import { prisma } from "../prisma/connection";
-
+import { Recipe, User } from "../app/generated/prisma/client";
 /**
  * Gets a user by their integer ID or hidden string ID.
  * @param name
  * @returns The user, or null
  */
-async function getUserbyPk(i: number) {
+async function getUserbyPk(i: number): Promise<User | null> {
   return prisma.user.findUnique({
     where: { pk: i },
   });
@@ -16,7 +16,7 @@ async function getUserbyPk(i: number) {
  * @param id
  * @returns The recipe, or null
  */
-async function getRecipeById(i: number) {
+async function getRecipeById(i: number): Promise<Recipe | null>{
   return prisma.recipe.findUnique({
     where: { id: i },
   });
@@ -26,6 +26,6 @@ async function getRecipeById(i: number) {
  * 
  * @returns All recipes in database
  */
-async function getAllRecipes() {
+async function getAllRecipes(): Promise<Recipe[] | null>{
   return prisma.recipe.findMany();
 }
