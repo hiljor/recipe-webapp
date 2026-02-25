ba34ace2da3379e7ccafa24ea2b3eff0b4c876f6
Hilde Jordal <xat013@uib.no>
2026-02-25 18:42:32 +0100

    recipes page shows recipe titles, completes #7

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,5 +1,21 @@
-import type { Recipe } from "../../app/generated/prisma/client";
+import type { Recipe } from "@/app/generated/prisma/client";
+import { getAllRecipes } from "@/lib/data"
+import { RecipeCard } from "@/ui/recipes/RecipeCard"
 
-export default function RecipesPage(
+export default async function RecipesPage() {
+  const recipes = await getAllRecipes();
 
-)
+  return (
+    <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+      {<div className="bg-white px-6">
+        {recipes.map((recipe) => {
+          return (
+            <div key={recipe.id}>
+              <RecipeCard recipe={recipe} />
+            </div>
+          );
+        })}
+      </div>}
+    </div>
+  );
+}
