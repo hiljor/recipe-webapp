8aa2343c86d666777416078df332860406c929f8
Hilde Jordal <xat013@uib.no>
2026-02-26 14:01:52 +0100

    removed dark mode and added global bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // Legg til bg-slate-50 og text-slate-900 her:
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50 text-slate-900 min-h-screen`}
       >
         {children}
       </body>
