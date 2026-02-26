8d403c908b11b5d5565cb5b8faca217dcfd58d46
Hilde Jordal <xat013@uib.no>
2026-02-26 13:04:01 +0100

    RecipeHeader component added icon

--- src/ui/recipes/Header.tsx
+++ src/ui/recipes/Header.tsx
@@ -1,17 +1,11 @@
 import { RecipeDTO } from "@/lib/types";
+import RecipeIcon from "./RecipeIcon";
 
 export default function RecipeHeader({ recipe }: { recipe: RecipeDTO }) {
   return (
     <div className="flex gap-6 items-start">
-      <div className="flex-shrink-0 w-48 w-48 overflow-hidden rounded-lg">
-        < Image />
-      </div>
+      <RecipeIcon src={recipe.imageUrl} />
       <div>
-        <h2 id="title">
-        </h2>
-        <p id="flavourText">
-
-        </p>
       </div>
     </div>
   );
