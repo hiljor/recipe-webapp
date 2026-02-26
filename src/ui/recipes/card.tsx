1bac1b99bbcdde47b3bcf13233966f1319b5bc52
Hilde Jordal <xat013@uib.no>
2026-02-26 14:32:01 +0100

    add icon to recipe card

--- src/ui/recipes/card.tsx
+++ src/ui/recipes/card.tsx
@@ -1,18 +1,22 @@
 import { Recipe } from "@/app/generated/prisma/client";
 import { RecipeDTO } from "@/lib/types";
 import Link from "next/link";
+import RecipeIcon from "./icon";
 
 export function RecipeCard({ recipe }: { recipe: RecipeDTO }) {
   return (
-    <div className="mt-4">
-      <Link href={`/recipes/${recipe.id}`}>
-        <h1 className="text-4xl text-black font-bold">{recipe.title}</h1>
-      </Link>
-      <div className="mt-2 text-m text-gray-600 flex items-center space-x-4">
-        {recipe.authorName && <span>by {recipe.authorName}</span>}
-        {recipe.added && (
-          <span>{new Date(recipe.added).toLocaleDateString()}</span>
-        )}
+    <div className="flex gap-6">
+      <RecipeIcon src={recipe.imageUrl}/>
+      <div className="mt-4">
+        <Link href={`/recipes/${recipe.id}`}>
+          <h1 className="text-4xl text-black font-bold">{recipe.title}</h1>
+        </Link>
+        <div className="mt-2 text-m text-gray-600 flex items-center space-x-4">
+          {recipe.authorName && <span>by {recipe.authorName}</span>}
+          {recipe.added && (
+            <span>{new Date(recipe.added).toLocaleDateString()}</span>
+          )}
+        </div>
       </div>
     </div>
   );
