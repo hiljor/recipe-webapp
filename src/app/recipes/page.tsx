d0e6fabd0441265187e897bb1a90c666ac5187e8
Hilde Jordal <xat013@uib.no>
2026-02-26 14:30:17 +0100

    Fix recipes page compoennt gap

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,4 +1,3 @@
-import type { Recipe } from "@/app/generated/prisma/client";
 import { getAllRecipes } from "@/lib/data"
 import { RecipeCard } from "@/ui/recipes/card"
 
@@ -7,14 +6,10 @@ export default async function RecipesPage() {
 
   return (
     <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-      {<div className="bg-white px-6">
-        {recipes.map((recipe) => {
-          return (
-            <div key={recipe.id}>
-              <RecipeCard recipe={recipe} />
-            </div>
-          );
-        })}
+      {<div className="flex flex-col gap-y-6">
+        {recipes.map((recipe) => (
+          <RecipeCard recipe={recipe} key={recipe.id} />
+        ))}
       </div>}
     </div>
   );
